7073d56f6a471b6ffbc500733570c129d2922a2f
Kunal Yelne <kunal17058@gmail.com>
2021-06-06 17:43:34 +0530

    Add fly functionality in abstract duck class

--- src/StrategyPattern/Duck.kt
+++ src/StrategyPattern/Duck.kt
@@ -3,5 +3,7 @@ package StrategyPattern
 abstract class Duck {
     fun quack() = println("Quack")
     fun swim() = println("Swim")
+    //Requirement came to add fly functionality for all ducks
+    fun fly() = println("Fly")
     abstract fun display()
 }
